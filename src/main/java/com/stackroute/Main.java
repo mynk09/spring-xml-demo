5f3d87831caa97b7d74662094ae2a3abc9c9bad1
mynk009 <mayank.r@cgi.com>
2019-09-25 14:47:23 +0530

    bean-lifecycle commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,20 +1,16 @@
 package com.stackroute;
 
-import com.stackroute.domain.Movie;
-import org.springframework.context.ApplicationContext;
+
+import com.stackroute.demo.BeanLifecycleDemoBean;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main
 {
-    public static void main( String[] args )
-    {
-        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
-        Movie mov1 = context.getBean("movie1", Movie.class);
-        mov1.role();
-
-        Movie mov2 = context.getBean("movie2", Movie.class);
-        mov2.role();
-
-        System.out.println(mov1==mov2);
+    public static void main( String[] args ) throws Exception {
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        BeanLifecycleDemoBean demoBean = context.getBean("lifecycledemo", BeanLifecycleDemoBean.class);
+        System.out.println("Life Cycle Demo");
+        context.close();
     }
 }
