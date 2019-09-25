2b1080761e102bc1115ca40cb34062210d064b4c
mynk009 <mayank.r@cgi.com>
2019-09-25 15:10:43 +0530

    bean-post-processor

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,16 +1,14 @@
 package com.stackroute.domain;
 
-import com.stackroute.domain.Actor;
+
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
 
-public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware{
+public class Movie {
+
     private Actor actor;
 
     public Movie(Actor actor) {
@@ -25,18 +23,4 @@ public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNam
         System.out.println(this.actor);
     }
 
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        System.out.println("Bean Factory has been created");
-    }
-
-    @Override
-    public void setBeanName(String s) {
-        System.out.println("Bean Name has been entered");
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println("Application context has been created");
-    }
 }
