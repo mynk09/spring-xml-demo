7cae99b2168e77048478cb8be182d2f50e008c3e
mynk009 <mayank.r@cgi.com>
2019-09-25 12:53:48 +0530

    branch commit

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -2,7 +2,15 @@ package com.stackroute.domain;
 
 import com.stackroute.domain.Actor;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware{
     private Actor actor;
 
     public Movie(Actor actor) {
@@ -15,5 +23,20 @@ public class Movie {
 
     public void role(){
         System.out.println(this.actor);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean Factory has been created");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean Name has been entered");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application context has been created");
     }
 }
